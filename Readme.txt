6d9b86402d6143cb39fd7f28c2191ecaa4165fba
rishab <rishabstudent2000@gmail.com>
2024-07-10 14:17:16 +0530

    Bottom tabs are done

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,4 @@
 ReactNative app
+setup is done
+File and Folder structure is done
+Bottom Tabs are done
--- app/index.jsx
+++ app/index.jsx
@@ -6,9 +6,9 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">aura!</Text>
+      <Text className="text-3xl font-pblack">Aura!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
+      <Link href="/home" style={{color:'blue'}}>Go to Home</Link>
     </View>
   );
 }
